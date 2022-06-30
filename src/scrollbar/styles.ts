88a58a05c4fa7409b73f0621ef62090eb5c03739
khanglvg <levogiakhang@gmail.com>
2022-06-30 17:21:52 +0700

    chore: add styles

---
+++ src/scrollbar/styles.ts
@@ -0,0 +1,27 @@
+export const containerStyleDefault = {
+	position: 'relative',
+	overflow: 'hidden',
+	width: '100%',
+	height: '100%',
+};
+
+export const contentViewStyleDefault = {
+	position: 'absolute',
+	top: 0,
+	left: 0,
+	right: 0,
+	bottom: 0,
+	overflow: 'scroll',
+	WebkitOverflowScrolling: 'touch',
+};
+
+export const verticalThumbStylesDefault = {
+	position: 'relative',
+	display: 'block',
+	width: '100%',
+};
+
+export const verticalTrackStylesDefault = {
+	position: 'absolute',
+	width: 6,
+};
