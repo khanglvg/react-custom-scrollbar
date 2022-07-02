5baf760cc122f6958ad56e0bf1a0375942ace206
khanglvg <levogiakhang@gmail.com>
2022-07-02 15:28:01 +0700

    chore: hide native axis scroll width

--- src/scrollbar/utils/getContentViewStyles.ts
+++ src/scrollbar/utils/getContentViewStyles.ts
@@ -1,7 +1,11 @@
 import { contentViewStyleDefault } from '../styles';
+import getScrollbarWidth from './getScrollbarWidth';
 
 export default function getContentViewStyles(): any {
+	const scrollbarWidth = getScrollbarWidth();
 	return {
 		...contentViewStyleDefault,
+		marginRight: scrollbarWidth ? -scrollbarWidth : 0,
+		marginBottom: scrollbarWidth ? -scrollbarWidth : 0,
 	};
 }
